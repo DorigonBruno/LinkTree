c43ad8ca20c31ea5aaa1b423f897f299ff91cdd5
Bruno Dorigon <brunodorigon@outlook.com>
2025-03-17 17:45:26 -0300

    build login page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,13 @@ const Home = () => {
   return (
     <div className="flex flex-col w-full h-screen py-4 items-center">
       <h1 className="text-3xl md:text-4xl font-bold text-white mt-20 ">
-        BrunoDev
+        Bruno Dorigon
       </h1>
       <span className="text-gray-300 mb-5 mt-3">Veja meus links ⬇️</span>
 
       <main className="flex flex-col w-11/12 max-w-xl items-center">
-        <section className="bg-white mb-4 w-full py-2 px-3 text-center rounded-lg select-none transition-transform hover:scale-105 duration-300 ease-in-out cursor-pointer">
-          <a href="">
+        <section className="bg-blue-600  text-white mb-4 w-full py-2 px-3 text-center rounded-lg select-none transition-transform  hover:scale-105 duration-300 ease-in-out cursor-pointer">
+          <a href="" target="blank">
             <p className="text-base md:text-lg">Linkedin</p>
           </a>
         </section>
